083089b5b4de5b96bda613150be6511c209043f4
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-04-30 08:22:20 +0000

    chore(release): v3.5.1

--- awscdkresourcesmongodbatlas/CfnAuditingProps.go
+++ awscdkresourcesmongodbatlas/CfnAuditingProps.go
@@ -4,7 +4,7 @@ package awscdkresourcesmongodbatlas
 // Returns and edits database auditing settings for MongoDB Cloud projects.
 type CfnAuditingProps struct {
 	// Unique 24-hexadecimal digit string that identifies your project.
-	GroupId *string `field:"required" json:"groupId" yaml:"groupId"`
+	ProjectId *string `field:"required" json:"projectId" yaml:"projectId"`
 	// Profile used to provide credentials information, (a secret with the cfn/atlas/profile/{Profile}, is required), if not provided default is used.
 	Profile *string `field:"optional" json:"profile" yaml:"profile"`
 }
