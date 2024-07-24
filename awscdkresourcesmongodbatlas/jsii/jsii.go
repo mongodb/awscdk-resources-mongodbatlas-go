7cd33c79cac6956c23a22b83760aaa2436c692b2
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-07-24 08:37:01 +0000

    chore(release): v3.7.2

--- awscdkresourcesmongodbatlas/jsii/jsii.go
+++ awscdkresourcesmongodbatlas/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed awscdk-resources-mongodbatlas-3.7.1.tgz
+//go:embed awscdk-resources-mongodbatlas-3.7.2.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("awscdk-resources-mongodbatlas", "3.7.1", tarball)
+	_jsii_.Load("awscdk-resources-mongodbatlas", "3.7.2", tarball)
 }
